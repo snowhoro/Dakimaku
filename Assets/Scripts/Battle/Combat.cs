db39551da549484a6dfe80fed5e05c436ad7cde6
snowhoro <snow.xs@gmail.com>
2015-11-10 20:30:09 -0300

    Reorder v2

---
+++ Assets/Scripts/Battle/Combat.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Combat : MonoBehaviour
+{
+    public GameObject dmgNumbers;
+
+    private static Combat _instance;
+    public static Combat instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<Combat>();
+
+                //DontDestroyOnLoad(_instance.gameObject);
+            }
+
+            return _instance;
+        }
+    }
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            //DontDestroyOnLoad(this);
+        }
+        else
+        {
+            if (this != _instance)
+                Destroy(this.gameObject);
+        }
+    }
+    public void Hit(BaseCharacter target, BaseCharacter origin)
+    {
+        target._currentHP -= origin._physicalBaseAttack;
+
+        if (target._currentHP <= 0)
+        {
+            target._currentHP = 0;
+            if (target.tag == "Enemy")
+            {
+                Destroy(target.gameObject, 0.5f);
+                //BattleList.Remove(target.gameObject);
+            }
+        }
+        //ShowDamage(target, origin.Strength);
+    }
+
+    private void ShowDamage(BaseCharacter target, int damage)
+    {
+        TextMesh textmesh = ((GameObject)Instantiate(dmgNumbers, target.transform.position + new Vector3(0f, 0.5f, -3f), Quaternion.identity)).GetComponent<TextMesh>();
+        textmesh.text = damage.ToString();
+
+        if (target.tag != "Player")
+            textmesh.color = Color.white;
+        else
+            textmesh.color = Color.red;       
+    }
+
+    private void ShowDamage(BaseCharacter target)
+    {
+        TextMesh textmesh = ((GameObject)Instantiate(dmgNumbers, target.transform.position + new Vector3(0f, 0.5f, -3f), Quaternion.identity)).GetComponent<TextMesh>();
+        textmesh.text = "MISS";
+        textmesh.color = Color.cyan;
+    }
+
+    public void Heal(BaseCharacter target, int hitPoints)
+    {
+        target._currentHP += hitPoints;
+        if (target._currentHP > target._maxBaseHP)
+            target._currentHP = target._maxBaseHP;
+    }
+    
+    public void CheckEnemiesAttacked()
+    {
+        CombatCheck combatCheck = new CombatCheck();
+        List<HitList> hitlist = combatCheck.GetEnemiesAttacked();
+        ShowBattle.instance.hitList = hitlist;
+
+        StartCoroutine(ShowBattle.instance.StartShowBattle());
+        /*foreach (HitList item in hitlist)
+        {
+            foreach (BaseCharacter victim in item.GetVictims())
+            {
+                ShowDamage(victim.gameObject);
+            }
+        }*/
+    }
+
+    public void CheckHeroesAttacked()
+    {
+        CombatCheck combatCheck = new CombatCheck();
+        List<HitList> hitlist = combatCheck.GetHeroesAttacked();
+        ShowBattle.instance.hitList = hitlist;
+        foreach (HitList item in hitlist)
+        {
+            ShowDamage(item.GetVictim().gameObject);
+        }
+    }
+
+    public void ShowDamage(GameObject obj)
+    {
+        GameObject aux = Instantiate(dmgNumbers);
+        aux.transform.SetParent(obj.transform.FindChild("Canvas"));
+        RectTransform rect = aux.GetComponent<RectTransform>();
+        rect.transform.localPosition = dmgNumbers.transform.localPosition;
+        rect.transform.localScale = dmgNumbers.transform.localScale;
+        int dmg = Random.Range(50, 70);
+        //MEJORAR!!! //////////////////////////////////////
+        obj.GetComponent<BaseCharacter>()._currentHP -= dmg;
+        ///////////////////////////////////////////////////
+
+        aux.GetComponent<Text>().text = dmg.ToString();
+        Destroy(aux, 1f);
+    }
+}
---
+++ Assets/Scripts/Battle/CombatCheck.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CombatCheck 
+{
+    private List<BaseCharacter> attackerList;
+    private List<BaseCharacter> friendList;
+    private HitList hitGroup;
+    private List<HitList> hitList;
+
+    public enum NextTo
+    {
+        None = 0,
+        Friend = 1,
+        Enemy = 2,
+    }
+    public CombatCheck()
+    {
+        hitList = new List<HitList>();
+    }
+    private void CheckAttack(BaseCharacter character)
+    {
+        hitGroup = new HitList();
+        hitGroup.AddVictim(character);
+
+        //Si el atacado es un Hero
+        if (character is Character)
+        {
+            attackerList = BattleList.instance.GetEnemies();
+            friendList = BattleList.instance.GetHeroes();
+        }
+        else
+        {
+            attackerList = BattleList.instance.GetHeroes();
+            friendList = BattleList.instance.GetEnemies();
+        }
+
+        //LEFT RIGHT
+        NextTo nextTo;
+        int auxPos = 0;
+        do
+        {
+            auxPos++;
+            nextTo = CheckDirection(character._gridPos, Vector2.left * auxPos);
+        } while (nextTo == NextTo.Friend);
+
+        if (nextTo == NextTo.Enemy)
+        {
+            auxPos = 0;
+            do
+            {
+                auxPos++;
+                nextTo = CheckDirection(character._gridPos, Vector2.right * auxPos);
+            } while (nextTo == NextTo.Friend);
+
+            if (nextTo == NextTo.Enemy)
+            {
+                //GUARDAR ACA LOS PARTICIPANTES
+                hitList.Add(hitGroup);
+            }
+        }
+
+        // UP DOWN
+        hitGroup = new HitList();
+        hitGroup.AddVictim(character);
+        auxPos = 0;
+        do
+        {
+            auxPos++;
+            nextTo = CheckDirection(character._gridPos, Vector2.up * auxPos);
+        } while (nextTo == NextTo.Friend);
+
+        if (nextTo == NextTo.Enemy)
+        {
+            auxPos = 0;
+            do
+            {
+                auxPos++;
+                nextTo = CheckDirection(character._gridPos, Vector2.down * auxPos);
+            } while (nextTo == NextTo.Friend);
+
+            if (nextTo == NextTo.Enemy)
+            {
+                //GUARDAR ACA LOS PARTICIPANTES
+                hitList.Add(hitGroup);
+            }
+        }
+    }
+    private NextTo CheckDirection(Vector2 targetPos, Vector2 direction)
+    {
+        foreach (BaseCharacter attacker in attackerList)
+        {
+            if (attacker._gridPos == targetPos + direction)
+            {
+                hitGroup.AddAttacker(attacker);
+                return NextTo.Enemy;
+            }
+        }
+
+        foreach (BaseCharacter friend in friendList)
+        {
+            if (friend._gridPos == targetPos + direction)
+                return NextTo.Friend;
+        }
+
+        return NextTo.None;
+    }
+    public List<HitList> GetEnemiesAttacked()
+    {
+        hitList.Clear();
+        List<BaseCharacter> enemiesList = BattleList.instance.GetEnemies();
+        foreach (BaseCharacter enemy in enemiesList)
+        {
+            CheckAttack(enemy);
+        }
+        Debug.Log("ANTES: " + hitList.Count);
+        Repaso();
+        Debug.Log("DESPUES: " + hitList.Count);
+        return hitList;
+    }
+    public List<HitList> GetHeroesAttacked()
+    {
+        hitList.Clear();
+        List<BaseCharacter> heroList = BattleList.instance.GetHeroes();
+        foreach (BaseCharacter hero in heroList)
+        {
+            CheckAttack(hero);
+        }
+        Debug.Log("ANTES: " + hitList.Count);
+        Repaso();
+        Debug.Log("DESPUES: " + hitList.Count);
+        return hitList;
+    }
+
+    public List<HitList> Repaso()
+    {
+        for (int i = 0; i < hitList.Count-1; i++)
+        {
+            for (int j = i+1; j < hitList.Count; j++)
+            {
+                Debug.Log("i " + i + " j " + j);
+                if (hitList[i].CheckSameAttackers(hitList[j]))
+                {
+                    Debug.Log("entre " + i + " " + j);
+                    hitList[i].AddVictim(hitList[j].GetVictim());
+                    hitList.RemoveAt(j);
+                }
+            }
+        }
+
+        return hitList;
+    }
+}
